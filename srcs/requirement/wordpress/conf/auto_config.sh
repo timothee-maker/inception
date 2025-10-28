ad5653638811837a5d5449bcc4a8ba4c798a45f2
tnolent <tnolent@student.42.fr>
2025-10-28 09:59:36 +0100

    changement nom volume + expose 9000 wp

--- srcs/requirement/wordpress/conf/auto_config.sh
+++ srcs/requirement/wordpress/conf/auto_config.sh
@@ -1,18 +1,35 @@
-sleep 10
-wp core download --allow-root
-wp config create	--allow-root \
-	--dbname=$SQL_DATABASE \
-	--dbuser=$SQL_USER \
-	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306
+#!/bin/bash
 
-wp core install \
-    --allow-root \
-    --url="${WP_PATH}" \
-    --title="${WP_TITLE}" \
-    --admin_user="${WP_ADMIN_USER}" \
-    --admin_password="${WP_ADMIN_PASS}" \
-    --admin_email="${WP_ADMIN_EMAIL}"
-    wp user create "${WP_USER}" "${WP_USER_EMAIL}" --role=author --user_pass="${WP_USER_PWD}" --allow-root
+# Attendre que MariaDB soit prêt
+sleep 10
+
+# Télécharger WordPress si nécessaire
+if [ ! -f /var/www/html/wp-config.php ]; then
+    cd /var/www/html
+    wp core download --allow-root
+    
+    wp config create --allow-root \
+        --dbname="$SQL_DATABASE" \
+        --dbuser="$SQL_USER" \
+        --dbpass="$SQL_PASSWORD" \
+        --dbhost=mariadb:3306
+    
+    wp core install --allow-root \
+        --url="$WP_PATH" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASS" \
+        --admin_email="$WP_ADMIN_EMAIL"
+    
+    wp user create "$WP_USER" "$WP_USER_EMAIL" \
+        --role=author \
+        --user_pass="$WP_USER_PWD" \
+        --allow-root
+fi
+
+# Fixer les permissions
 chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+# Démarrer PHP-FPM
 exec php-fpm8.2 -F
